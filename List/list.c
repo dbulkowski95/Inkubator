4572ac0f634792dc0406d86b3756de9b0ddc5e59
dbulkowski95 <d.bulkowski95@gmail.com>
2018-10-25 14:21:36 +0200

    List with clear() function

--- List/list.c
+++ List/list.c
@@ -12,11 +12,11 @@ typedef struct{
 }list;
 
 void headGraph(int nodeValue, node* ptr){
-	printf("________________________\n");
+	printf("-------------------------\n");
 	printf("|\t\t\t|\n");
 
-	printf("|Head->value: %d\t|\n",nodeValue);
-	printf("|Head->ptr: %p\t|\n",ptr);
+	printf("|Head->value: %10.2d|\n",nodeValue);
+	printf("|H->ptr:\t%p|\n",ptr);
 
 	printf("|\t\t\t|\n");
 	printf("-------------------------\n");
@@ -29,14 +29,14 @@ void nodeArrow(){
 	printf("\tv\n");
 }
 
-void printlist(list* ptrToNode){
-	printf("%p\n", ptrToNode->head);
-}
-
-void printNode(node* ptrToNode){
-	printf("%d\n", ptrToNode->value);
-	printf("%p\n", ptrToNode->nextNode);
-}
+//void printlist(list* ptrToNode){
+//	printf("%p\n", ptrToNode->head);
+//}
+//
+//void printNode(node* ptrToNode){
+//	printf("%d\n", ptrToNode->value);
+//	printf("%p\n", ptrToNode->nextNode);
+//}
 
 int listPush(list* _pList, size_t data){
 	assert(_pList != NULL); //precondition
@@ -48,8 +48,8 @@ int listPush(list* _pList, size_t data){
 		_pList->head->nextNode = NULL;
 //		printlist(_pList);
 //		printNode(_pList->head);
-		headGraph(_pList->head->value,_pList->head);
-		nodeArrow();
+//		headGraph(_pList->head->value,_pList->head);
+//		nodeArrow();
 		return 0;
 	}
 //	currentNode = _pList->head;
@@ -58,22 +58,46 @@ int listPush(list* _pList, size_t data){
 	currentNode->nextNode = malloc(sizeof(currentNode));
 	while(currentNode->nextNode != NULL){
 		currentNode->value = data;
-		headGraph(currentNode->value,(node*)currentNode->nextNode);
-		nodeArrow();
+//		headGraph(currentNode->value,(node*)currentNode->nextNode);
+//		nodeArrow();
 		currentNode = (node*)currentNode->nextNode;
 		return 0;
 	}
 	return 0;
 }
 
+void clearNode(list* _pList){
+	node *clearNodes = _pList->head;
+	node *_tempNode = NULL;
+	while((clearNodes->nextNode != NULL)){
+//		assert(clearNodes->nextNode == NULL);
+		if(clearNodes->nextNode == NULL){
+			_tempNode = clearNodes;
+			clearNodes = NULL;
+			free(clearNodes);
+		}
+		else{
+			clearNodes = (node*)clearNodes->nextNode;
+		}
+	clearNodes = _tempNode;
+	}
+}
+
+//void print(list* _pPrintList){
+//	node *printList = _pPrintList->head;
+//
+//}
 
 int main(){
 	list *_hPtr = NULL;
 	_hPtr = malloc(sizeof(list));
-	printf("|Head MAIN->ptr: %p\t|\n",_hPtr);
 	listPush(_hPtr,10);
+//	clearNode(_hPtr->head);
 	listPush(_hPtr,20);
-	listPush(_hPtr,30);
+//	clearNode(_hPtr->head);
+//	listPush(_hPtr,30);
+	clearNode(_hPtr);
+//	clearNode(_hPtr->head);
 //	listPush(_hPtr,40.5);
 //	listPush(_hPtr,'c');
 	return 0;
